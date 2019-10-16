736fcb7b480cb1d42f012466fb4d1dd61cdac417
vivianhuynh91827 <vhuynh00@stuy.edu>
2019-10-16 13:53:48 -0400

    functions completed

--- struct.c
+++ struct.c
@@ -1,17 +1,25 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <time.h>
 #include "struct.h"
 
 void example(){
-  
+  char* names[] = {"unicorn879","fireboy839",
+                  "epic345","xxhelloxx", "santaclaus234","12bear34"};
+  srand(time(NULL));
+  int ind = rand() % 6;
+  struct player ex;
+  ex.name = names[ind];
+  ex.level = (rand() % 100) + 1;
+  printf("Example player: {%d, %s}\n",ex.level,ex.name);
 }
 
 void toString(struct player person){
-
+  printf("Player's Username: %s\n", person.name);
+  printf("Player's Level: %d\n", person.level);
 }
-void set(char newName[], int newLevel){
 
-}
-int main(){
-  example();
+void set(struct player person, char* newName, int newLevel){
+  person.name = newName;
+  person.level = newLevel;
 }
