98d721ff34a69ad0a0be69879de29671b2072098
vivianhuynh91827 <vhuynh00@stuy.edu>
2019-10-17 11:41:29 -0400

    added srand to main

--- main.c
+++ main.c
@@ -1,8 +1,10 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include "struct.h"
+#include <time.h>
 
 int main(){
+  srand(time(NULL));
   printf("------------START------------\n");
   printf("\n");
   printf("Examples:\n");
